ef442aa36c0eb6194856e1f0291aede89124098a
Daniel <dydaniely@gmail.com>
2013-10-05 19:37:57 +0300

    reportSetting Model Class

--- src/com/example/Vstockmonitoring/ui/EditReportSetting.java
+++ src/com/example/Vstockmonitoring/ui/EditReportSetting.java
@@ -1,9 +1,17 @@
 package com.example.Vstockmonitoring.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.Vstockmonitoring.MenuActivity;
 import com.example.Vstockmonitoring.R;
 import com.example.Vstockmonitoring.adapter.ReportSettingAdapter;
 import com.example.Vstockmonitoring.model.ReportSetting;
@@ -47,8 +55,70 @@ public class EditReportSetting extends Activity {
     }
 
     private void loadActivityItemValue() {
-
+        report_id=Integer.valueOf(getIntent().getExtras().getString("reportSetting_id"));
+        media=getIntent().getExtras().getString("reportSetting_media");
+        phone=Integer.valueOf(getIntent().getExtras().getString("reportSetting_phone"));
+        address=getIntent().getExtras().getString("reportSetting_url");
+        period=getIntent().getExtras().getString("reportSetting_period");
+        initControls();
+        report_media.setText(report_media.toString(), TextView.BufferType.EDITABLE);
+        report_period.setText(report_period.toString(), TextView.BufferType.EDITABLE);
+        report_phone.setText(report_phone.toString(), TextView.BufferType.EDITABLE);
+        url_address.setText(address.toString(), TextView.BufferType.EDITABLE);
 
     }
+    public boolean onCreateOptionsMenu(Menu menu)   {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.modifydelete,menu);
+        inflater.inflate(R.menu.home,menu);
+        return  true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)   {
+        switch(item.getItemId()){
+            case R.id.nUpdate:
+                editReportSetting(findViewById(R.id.nUpdate));
+                return true;
+            case R.id.nDelete:
+                deleteReportSetting(findViewById(R.id.nDelete));
+                return true;
+            case R.id.nHome:
+                Intent intent = new Intent(this, MenuActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                this.startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void deleteReportSetting(View viewById) {
+
+
+        Toast.makeText(getApplicationContext(),"Data Deletion is Not Allowed",Toast.LENGTH_LONG).show();
+    }
+
+    private void editReportSetting(View viewById) {
+        ReportSettingAdapter dbHelper=new ReportSettingAdapter(this);
+        dbHelper.open();
+        reportSetting=new ReportSetting();
+        reportSetting.setReport_media(report_media.getText().toString());
+        reportSetting.setReport_phone(report_phone.getText().toString());
+        reportSetting.setUrl_address(url_address.getText().toString());
+        reportSetting.setReport_period(report_period.getText().toString());
+        boolean status;
+        status=dbHelper.updateReportSetting(report_id,reportSetting.getReport_media(),Integer.valueOf(reportSetting.getReport_phone()),reportSetting.getUrl_address(),reportSetting.getReport_period());
+        if (status!=false) {
+            Toast.makeText(getApplicationContext(),"Data Successfully Updated  ",Toast.LENGTH_SHORT).show();
+            dbHelper.close();
+            finish();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Data is not Successfully Updated ",Toast.LENGTH_SHORT).show();
+            dbHelper.close();
+            finish();
+        }
+    }
 }
---
+++ src/com/example/Vstockmonitoring/ui/ReportSettingListActivity.java
@@ -0,0 +1,109 @@
+package com.example.Vstockmonitoring.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.example.Vstockmonitoring.MenuActivity;
+import com.example.Vstockmonitoring.R;
+import com.example.Vstockmonitoring.adapter.ReportSettingAdapter;
+import com.example.Vstockmonitoring.customAdapter.ReportSettingsListAdapter;
+import com.example.Vstockmonitoring.model.ReportSetting;
+
+import java.util.ArrayList;
+
+/**
+ * Created by DanielY on 10/5/13.
+ */
+public class ReportSettingListActivity extends Activity implements AdapterView.OnItemClickListener{
+
+    private ReportSetting reportSetting;
+    private ReportSettingAdapter dbHelper ;
+    private ArrayList<ReportSetting>  reportSettings;
+
+    public void onCreate(Bundle savedInstance){
+       super.onCreate(savedInstance);
+        setContentView(R.layout.reportsettinglist);
+        displayReportSetting();
+        ListView listView=(ListView)findViewById(R.id.listView1);
+        listView.setOnItemClickListener(this);
+    }
+
+    private void displayReportSetting() {
+        try {
+            dbHelper=new ReportSettingAdapter(this);
+            dbHelper.open();
+            final ListView  listview = (ListView) findViewById(R.id.listView1);
+            Cursor results;
+            results=dbHelper.fetchAllReportSetting();
+            if (results.getCount()!=-1){
+            reportSettings= new ArrayList<ReportSetting>();
+                while (results.moveToNext()){
+                    reportSetting = new ReportSetting();
+                    reportSetting.setId(results.getString(0));
+                    reportSetting.setReport_media(results.getString(1));
+                    reportSetting.setReport_phone(results.getString(2));
+                    reportSetting.setUrl_address(results.getString(3));
+                    reportSetting.setReport_period(results.getString(4));
+                    reportSettings.add(reportSetting);
+                }
+                ReportSettingsListAdapter reportSettingsListAdapter= new ReportSettingsListAdapter(this,R.layout.reportsettinglist,reportSettings);
+                listview.setAdapter(reportSettingsListAdapter);
+                results.close();
+                dbHelper.close();
+            }
+
+        }
+        catch (Exception ex){
+            Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.addnew,menu);
+        inflater.inflate(R.menu.home,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.nAdd :
+                Intent intent=new Intent(this,CreateReportSettingActivity.class )   ;
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                this.startActivity(intent);
+                return true;
+            case R.id.nHome:
+                Intent homeIntent = new Intent(this, MenuActivity.class);
+                homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                this.startActivity(homeIntent);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long l) {
+
+        reportSetting=new ReportSetting();
+        reportSetting=(ReportSetting)parent.getItemAtPosition(position);
+        Intent intent=new Intent(this.getApplicationContext(),EditReportSetting.class);
+        intent.putExtra("reportSetting_id",reportSetting.getId());
+        intent.putExtra("reportSetting_phone",reportSetting.getReport_phone());
+        intent.putExtra("reportSetting_media",reportSetting.getReport_media());
+        intent.putExtra("reportSetting_url",reportSetting.getUrl_address());
+        intent.putExtra("reportSetting_period",reportSetting.getReport_period());
+        this.startActivity(intent);
+    }
+}
