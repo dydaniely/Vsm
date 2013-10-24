2901231bb13c3dc0a65ffeea56aae53386f0a76d
Daniel <dydaniely@gmail.com>
2013-10-24 11:18:59 +0300

    Editing

--- src/com/example/Vstockmonitoring/ui/EditReceivedItems.java
+++ src/com/example/Vstockmonitoring/ui/EditReceivedItems.java
@@ -2,11 +2,13 @@ package com.example.Vstockmonitoring.ui;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -14,9 +16,12 @@ import android.widget.Toast;
 
 import com.example.Vstockmonitoring.MenuActivity;
 import com.example.Vstockmonitoring.R;
+import com.example.Vstockmonitoring.adapter.SupplierAdapter;
 import com.example.Vstockmonitoring.adapter.VaccineDetailAdapter;
 import com.example.Vstockmonitoring.model.VaccineDetails;
 
+import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -25,7 +30,7 @@ import java.util.Date;
 public class EditReceivedItems extends Activity {
     private VaccineDetails vaccineDetails;
     private VaccineDetailAdapter detailAdapter;
-
+    ArrayList<String> arrayList= new ArrayList<String>();
    private int  vaccine_id;
    private String  vaccineName;
    private static int  supplier_id;
@@ -52,6 +57,7 @@ public class EditReceivedItems extends Activity {
         setContentView(R.layout.editreceiveditems);
         initControls();
         LoadActivityValue();
+        LoadSpinner();
     }
 
     private void LoadActivityValue() {
@@ -59,6 +65,7 @@ public class EditReceivedItems extends Activity {
         //load Received Date and set all controls value
         receivedDate=getIntent().getExtras().getString("receivedDate");
         supplier_id=getIntent().getExtras().getInt("supplierId");
+
         vaccine_id= getIntent().getExtras().getInt("vaccine_id");
         vaccineName=getIntent().getExtras().getString("vaccine_name");
         batchNo=getIntent().getExtras().getString("batchNo");
@@ -74,8 +81,31 @@ public class EditReceivedItems extends Activity {
         presentation_dose_per_vials.setText(presentationDosePerVials.toString(), TextView.BufferType.EDITABLE);
         quantity_on_hand.setText(String.valueOf(quantityOnHand), TextView.BufferType.EDITABLE);
         manufacturer.setText(manufacture.toString(), TextView.BufferType.EDITABLE);
-        //vaccine_vvm.setLabelFor( );
-        //supplier
+        supplier_Info.setSelection(0);
+        vaccine_vvm.setSelection(0);
+
+
+    }
+
+    private void LoadSpinner() {
+        SupplierAdapter dbHelper;
+        final Spinner spinner = (Spinner)   findViewById(R.id.nEditSupplierSpinner)  ;
+        dbHelper=new SupplierAdapter(this);
+        dbHelper.open();
+        Cursor results= dbHelper.fetchAllSupplier();
+        if(results.getCount()!=-1){
+            while(results.moveToNext())
+            {
+                arrayList.add(results.getString(1));
+            }
+        }
+        ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,arrayList );
+        stringArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(stringArrayAdapter);
+        //dbHelper.open();
+         results.close();
+        dbHelper.close();
     }
 
     private void initControls() {
@@ -117,6 +147,24 @@ public class EditReceivedItems extends Activity {
     }
 
     private void editReceivedItems(View viewById) {
+        detailAdapter=new VaccineDetailAdapter(this);
+        detailAdapter.open();
+        long status;
+        vaccineDetails.setBatch_no(batch_no.getText().toString());
+        vaccineDetails.setManufacturer(manufacturer.getText().toString());
+        vaccineDetails.setQuantity_on_hand(Integer.valueOf(quantity_on_hand.getText().toString()));
+        vaccineDetails.setPresentation_dose_vials(presentation_dose_per_vials.getText().toString());
+        vaccineDetails.setExpiry_date(expiry_date.getText().toString());
+        vaccineDetails.setVaccine_id(String.valueOf(vaccine_id));
+
+        vaccineDetails.setDate(DateFormat.getDateInstance().format(new Date()));//ReceivingDate
+        status=detailAdapter.createVaccineDetail(vaccineDetails.getVaccine_id(), vaccineDetails.getSupplier_id() ,
+                vaccineDetails.getBatch_no(),  vaccineDetails.getExpiry_date(),  vaccineDetails.getPresentation_dose_vials(),  vaccineDetails.getVaccine_vvm(),  vaccineDetails.getQuantity_on_hand(),vaccineDetails.getManufacturer());
+        if (status!=-1){
+            Toast.makeText(getApplicationContext(), "data saved Successfully", Toast.LENGTH_LONG).show();
+            finish();
+        }
+        detailAdapter.close();
     }
 
     private void deleteReceivedItems(View viewById) {
